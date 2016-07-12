dbf58b603d3b7fe857ad1dc6b96c95890fe9126e
chi <heysmile1210@hotmail.com>
2016-07-12 18:15:56 +0800

    Add button click event

--- Project 02 StackView/Project 02 StackView/ViewController.swift
+++ Project 02 StackView/Project 02 StackView/ViewController.swift
@@ -13,13 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // TODO image fit aspect
 
-
+    @IBAction func openFacebookURL(){
+        let url = NSURL(string: "https://www.facebook.com/chi.kuo.777")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    @IBAction func openGithubURL(){
+        let url = NSURL(string: "https://github.com/ChiKuo")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    @IBAction func openLinkedInURL(){
+        let url = NSURL(string: "https://www.linkedin.com/in/chi-kuo-99596781")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
 }
 
